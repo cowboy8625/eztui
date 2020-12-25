478198be3910e280c68b1f681ebb4bcc9a47438a
cowboy8625 <43012445+cowboy8625@users.noreply.github.com>
2020-12-25 16:02:07 -0600

    fixed hello world example

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -3,11 +3,11 @@ use eztui::*;
 
 fn main() {
     let mut stdout = stdout();
-    let mut window = eztui::Window::new(0, 0, 10, 10);
-    window.set_text(0, "Hello World\nNew lines also work like normal");
-    window.set_pos(50, 10);
-    // FIXME: Remove clear some how.
-    eztui::clear(&mut stdout);
+    let (width, height) = (40, 20);
+    let text = "Hello World";
+    let mut window = eztui::Window::new(10, 10, width, height);
+    window.set_background_color(Color::Rgb{r:0, g:139, b:139});
+    window.set_text(width/2-(text.len()as u16)/2, height/2, text, Color::Red, Color::Blue);
     window.draw(&mut stdout);
     stdout.flush().expect("This should flush");
 }
